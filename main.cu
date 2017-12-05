89a8f39b8b7a87cf43427dd9fd2f448c71eb79d6
miyagi <rapurasusann@yahoo.co.jp>
2017-12-05 10:28:05 +0900

    Update main.cu

--- main.cu
+++ main.cu
@@ -1,4 +1,3 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
@@ -8,11 +7,18 @@
 #include "sphere.h"
 
 
-#define HEIGHT 400
+#define HEIGHT 400            
 #define WIDTH 800
 #define SPHERES 3
-#define SIZE HEIGHT*WIDTH
-#define DEEP 32
+#define SIZE (HEIGHT*WIDTH)
+#define DEEP 100
+#define THREAD 16
+
+unsigned char *pixels;        //pixelの色を格納する配列へのポインタ(ホスト用)
+unsigned char *dev_pixels;    //pixelの色を格納する配列へのポインタ(デバイス用)
+float *temp_pixels;           //pixelの色を格納する配列へのポインタ(平均化用)
+
+sphere *list;                 //球の情報を格納する配列へのポインタ
 
 __constant__ sphere dev_list[SPHERES];
 
@@ -74,16 +80,17 @@ __device__ vec3 color(ray&r, sphere *list, int depth, hit_record rec)//色を設
 	}
 }
 
-__global__ void kernel(unsigned char *ptr) {
-	__shared__ vec3 col[DEEP];
+__global__ void kernel(float *ptr) {
+	vec3 col;
 
-	float x = ((float)blockIdx.x + XorFrand(0.0, 1.0)) / (float)WIDTH;
-	float y = ((float)blockIdx.y + XorFrand(0.0, 1.0)) / (float)HEIGHT;
+	int x = threadIdx.x + blockIdx.x*blockDim.x;
+	int y = threadIdx.y + blockIdx.y*blockDim.y;
 
-	int offset = blockIdx.x + blockIdx.y * gridDim.x;
-	int cacheIndex = threadIdx.x;
+	float w = ((float)x + XorFrand(0.0, 1.0)) / (float)WIDTH;
+	float h = ((float)y + XorFrand(0.0, 1.0)) / (float)HEIGHT;
 
-	vec3 lookfrom(0,0, -7);
+	int offset = x + y *blockDim.x* gridDim.x;
+	vec3 lookfrom(0, 0, -7);
 	vec3 lookat(0, 0, 0);
 	float dist_to_foucus = (lookfrom - lookat).length();
 	float aperture = 0.0;
@@ -94,7 +101,7 @@ __global__ void kernel(unsigned char *ptr) {
 	hit_record rec;
 	hit_record temp_rec;
 
-	ray r = cam.get_ray(x, y);//カメラクラスに問題あり
+	ray r = cam.get_ray(w, h);
 	ray temp_r = r;
 
 	for (int i = 0; i < 15; i++) {
@@ -106,60 +113,64 @@ __global__ void kernel(unsigned char *ptr) {
 	}
 
 	if (depth == 0) {//当たらなければ背景を描画
-		col[cacheIndex] = DrawBack(r);
+		col = DrawBack(r);
 	}
 
 	else if (shadow == false) {
-		col[cacheIndex] = color(r, dev_list, depth, temp_rec);//vec3型変数colに、関数colorで出した値を入れる
+		col = color(r, dev_list, depth, temp_rec);//vec3型変数colに、関数colorで出した値を入れる
 	}
 	else {
-		col[cacheIndex] = vec3(1.0, 0.0, 0.0);
+		col = vec3(1.0, 0.0, 0.0);
 	}
 
-	int i = blockDim.x / 2;
+	col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
 
-	while (i != 0) {
-		if (cacheIndex < i) {
-			col[cacheIndex] += col[cacheIndex + i];
-		}
-		i /= 2;
-	}
-
-	if (cacheIndex == 0) {
-		col[cacheIndex] /= (float)DEEP;
-		col[cacheIndex] = vec3(sqrt(col[cacheIndex].r()), sqrt(col[cacheIndex].g()), sqrt(col[cacheIndex].b()));
-		ptr[offset * 3 + 0] = int(255.99*col[cacheIndex].r());
-		ptr[offset * 3 + 1] = int(255.99*col[cacheIndex].g());
-		ptr[offset * 3 + 2] = int(255.99*col[cacheIndex].b());
-	}
+	ptr[offset * 3 + 0] += col.r() / DEEP;
+	ptr[offset * 3 + 1] += col.g() / DEEP;
+	ptr[offset * 3 + 2] += col.b() / DEEP;
 }
 
-void display() {
-	unsigned char *pixels;
-	pixels = new unsigned char[WIDTH*HEIGHT * 3];
+__global__ void conversion(float *a, unsigned char *b) {
+	int index = blockIdx.x;
+	b[index * 3 + 0] = (int)(255.99 *a[index * 3 + 0]);
+	b[index * 3 + 1] = (int)(255.99 *a[index * 3 + 1]);
+	b[index * 3 + 2] = (int)(255.99 *a[index * 3 + 2]);
+}
 
-	unsigned char *dev_pixels;
-	cudaMalloc((void**)&dev_pixels, WIDTH*HEIGHT * 3);
-	sphere *list;
+void Initialize() {
+	pixels = new unsigned char[SIZE * 3];
+
+	cudaMalloc((void**)&dev_pixels, SIZE * 3);
+	cudaMalloc((void**)&temp_pixels, (sizeof(float))*(SIZE * 3));
+
 	list = new sphere[SPHERES];
 	list[0] = sphere(vec3(0, 0, -1), 0.5, vec3(0.1, 0.1, 0.8), lambertian);
 	list[1] = sphere(vec3(1, 0, -1), 0.5, vec3(1.0, 1.0, 0.3), metal);
 	list[2] = sphere(vec3(0, -100.5, -1), 100, vec3(0.3, 0.8, 0.3), lambertian);
-	
+
 	cudaMemcpyToSymbol(dev_list, list, sizeof(sphere)*SPHERES);
-	
-	dim3 grids(WIDTH, HEIGHT);
-	kernel << <grids, DEEP >> > (dev_pixels);
-	
+
+	dim3 grids(WIDTH/THREAD, HEIGHT/THREAD);
+	dim3 threads(THREAD, THREAD);
+
+	for (int i = 0; i < DEEP; i++) {
+		kernel << <grids, threads >> > (temp_pixels);
+	}
+	conversion << <SIZE, 1 >> > (temp_pixels, dev_pixels);
+
 	cudaMemcpy(pixels, dev_pixels, SIZE * 3, cudaMemcpyDeviceToHost);
 
+}
+
+void display() {
 	glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 	glFlush();
+}
 
+void atexit() {
 	cudaFree(dev_pixels);
 	delete[] pixels;
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -167,8 +178,9 @@ int main(int argc, char *argv[])
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInit(&argc, argv);
 	glutCreateWindow(argv[0]);
+	Initialize();
 	glutDisplayFunc(display);
 	glutMainLoop();
+	atexit();
 	return 0;
 }
-
